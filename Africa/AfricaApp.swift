3e0f2b168da15fa3193529af880c3542ec464f6c
vladimir <volt901@icloud.com>
2021-08-30 21:40:57 +0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Владимир Ширяев on 30.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
