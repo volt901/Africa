e2e8fa079e0eb26e675a3a46f2548cebf896ef2c
vladimir <volt901@icloud.com>
2021-09-09 22:10:19 +0500

    0.4

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         // MARK: - Properties
+        let animals: [Animal] = Bundle.main.decode("animals.json")
         
         // MARK: - Body
         NavigationView {
@@ -17,6 +18,10 @@ struct ContentView: View {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                ForEach(animals) {animal in
+                    AnimalListItemView(animal: animal)
+                }
+                
             }// : List
             .navigationBarTitle("Africa", displayMode: .large)
         }//: Navigator
