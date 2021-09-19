385fa22c2139f12b359abbd734392a685f40773d
vladimir <volt901@icloud.com>
2021-09-19 22:10:25 +0500

    0.9

--- Africa/View/MapAnnotationView.swift
+++ Africa/View/MapAnnotationView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct MapAnnotationView: View {
+    var location: NationParkLocation
+    
+    @State private var animation: Double = 0.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+                .fill(Color.accentColor)
+                .frame(width: 54, height: 54, alignment: .center)
+            
+            Circle()
+                .stroke(Color.accentColor,lineWidth: 2)
+                .frame(width: 52, height: 52, alignment: .center)
+                .scaleEffect(1+CGFloat(animation))
+                .opacity(1-animation)
+
+            Image(location.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 48, height: 48)
+                .clipShape(Circle())
+        }//:ZSTACK
+        .onAppear{
+            withAnimation(Animation.easeOut(duration: 2).repeatForever(autoreverses: false)){
+                animation = 1
+            }
+            
+        }
     }
 }
 
 struct MapAnnotationView_Previews: PreviewProvider {
+    static var locations: [NationParkLocation] = Bundle.main.decode("locations.json")
     static var previews: some View {
-        MapAnnotationView()
+        MapAnnotationView(location: locations[0])
             .previewLayout(.sizeThatFits)
             .padding()
     }
