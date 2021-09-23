85f69a30db157f80b0af26aaeab79ddd2763c009
vladimir <volt901@icloud.com>
2021-09-23 22:19:54 +0500

    0.11

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -7,12 +7,66 @@
 
 import SwiftUI
 
+
 struct GalleryView: View {
+    @State private var selectedAnimal: String = "lion"
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptic = UIImpactFeedbackGenerator(style: .medium)
+
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+    
+//    let gridLayout: [GridItem] = Array (repeating: GridItem(.flexible()), count: 3)
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn : Double = 3.0
+    
+    func gridSwitch(){
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+    
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack(alignment: .center, spacing: 30 ){
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                Slider (value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: { value in
+                        gridSwitch()
+                    })
+
+                
+                //MARK: -GRID
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptic.impactOccurred()
+                            }
+                    } //: LOOP
+                } //: GRID
+                .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
+                .onAppear(perform: {
+                    gridSwitch()
+                })
+            }// :VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical,50)
         }//: SCROLL
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
     }
 }
