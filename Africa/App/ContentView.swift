733114eda5d9a493e6e57ad499c2477e08273b43
vladimir <volt901@icloud.com>
2021-09-25 23:52:23 +0500

    0.12

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -8,24 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptic = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var isGridViewActive: Bool = false
+    
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+    
     var body: some View {
         // MARK: - Properties
-        let animals: [Animal] = Bundle.main.decode("animals.json")
+
         
         // MARK: - Body
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                ForEach(animals) {animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)){
-                        AnimalListItemView(animal: animal)
-                        }// : Link
-                }
-                
-            }// : List
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        ForEach(animals) {animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)){
+                                AnimalListItemView(animal: animal)
+                            }// : Link
+                        } //: LOOP
+                    } //: LIST
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            Text("Grid view is active")
+                        } //: GRID
+                    }//: SCROLL
+                }// : List
+            } //: GROUP
+            
+            
+            
             .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16){
+                        //: LIST
+                        Button(action: {
+                            print("List view is activated")
+                            isGridViewActive = false
+                            haptic.impactOccurred()
+                        }){
+                            Image (systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+                        
+                        // GRID
+                        Button(action: {
+                            print("Grid view is activated")
+                            isGridViewActive = true
+                            haptic.impactOccurred()
+                        }){
+                            Image (systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+                        
+                        
+                        
+                        
+                    }//: HSTACK
+                } //: BUTTONS
+            } //: TOOLBAR
+
         }//: Navigator
     }
 }
