d23878d1f394d986255aa3059e0d35127f20c14c
vladimir <volt901@icloud.com>
2021-09-26 15:54:11 +0500

    1

---
+++ Africa/View/CreditsView.swift
@@ -0,0 +1,28 @@
+//
+//  CreditsView.swift
+//  Africa
+//
+//  Created by Владимир Ширяев on 26.09.2021.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        Text("""
+Copyright Vova
+ALL right reserver
+Better Apps ♡ Less Code
+
+
+""")
+    }
+}
+
+struct CreditsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditsView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
